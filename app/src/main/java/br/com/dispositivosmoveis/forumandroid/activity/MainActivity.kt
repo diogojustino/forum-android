e88c34664ac25c1e08d351c9f769e6738f4c4191
Diogo Leite <diogo.development@gmail.com>
2018-10-02 15:04:55 -0300

    Criado main Activity

---
+++ app/src/main/java/br/com/dispositivosmoveis/forumandroid/activity/MainActivity.kt
@@ -0,0 +1,13 @@
+package br.com.dispositivosmoveis.forumandroid.activity
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import br.com.dispositivosmoveis.forumandroid.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
