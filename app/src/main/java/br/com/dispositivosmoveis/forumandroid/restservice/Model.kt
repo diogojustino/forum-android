fc38c3c16ba08a48fd39bf96adc505ebc5645715
Roberto <rvercelio@gmail.com>
2018-10-03 16:30:29 -0300

    coloquei os models

---
+++ app/src/main/java/br/com/dispositivosmoveis/forumandroid/restservice/Model.kt
@@ -0,0 +1,12 @@
+package com.moveis.forum.restservice
+
+import java.io.Serializable
+import java.util.*
+
+
+data class Categoria(var id: Int? = null, var nome: String?) : Serializable
+
+data class Topico(var id: Int? = null, var titulo: String?, var autor: String?, var descricao: String?,
+                  var categoria: Int?, var created_at: Date? = null, var updated_at: Date? = null) : Serializable
+
+data class Comentario(var id: Int? = null, var descricao :String? var categoria: Int?, var topico: Int?) : Serializable
