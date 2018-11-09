e7d325762f7ea45914645ef3e9398a3e89771f1f
Unknown <yarlley.silva@gmail.com>
2018-11-09 16:04:59 -0200

    Melhorias em MainActivity

--- app/src/main/java/br/com/dispositivosmoveis/forumandroid/activity/MainActivity.kt
+++ app/src/main/java/br/com/dispositivosmoveis/forumandroid/activity/MainActivity.kt
@@ -3,12 +3,19 @@ package br.com.dispositivosmoveis.forumandroid.activity
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.ArrayAdapter
+import android.widget.ListAdapter
+import android.widget.ListView
 import android.widget.Toast
 import br.com.dispositivosmoveis.forumandroid.R
 //import br.com.dispositivosmoveis.forumandroid.adapter.ListCategoriaAdapter
 import com.moveis.forum.restservice.*
+import java.lang.reflect.Array
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var listView: ListView
+    private val cate: List<Categoria>? = null
 
     private var categorias: List<Categoria>? = null
     private var topicos: List<Topico>? = null
@@ -16,29 +23,58 @@ class MainActivity : AppCompatActivity() {
     private val TAG = "MAIN"
     //var listCategoriaAdapter: ListCategoriaAdapter? = null
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         Log.i(TAG, "onCreate")
-    }
 
-    //IRÁ PEGAR OS DADOS NA API E APENAS EXIBIR RÁPIDAMENTE COMO SE FOSSE UM POP-UP
-    override fun onStart(){
-        super.onStart();
-
-        ForumWebClient().getCategorias(object : ICallbackResponse<List<Categoria>>{
+        ForumWebClient().getCategorias(object : ICallbackResponse<List<Categoria>> {
             override fun success(categorias: List<Categoria>) {
                 this@MainActivity.categorias = categorias
 
-                for (cat in categorias!!){
-                    alerta(cat.toString())
+                for (cat in categorias!!) {
+                    //alerta(cat.nome!!)
                     Log.i(TAG + "Categoria", cat.toString())
+
+                    listView = findViewById(R.id.listView)
+
+
+                    val categorias = arrayOf(cat.nome)
+
+                    listView.adapter = ArrayAdapter<String>(this@MainActivity, android.R.layout.simple_list_item_1, categorias)
+
+
                 }
+
+
             }
         })
+
+
     }
 
-    fun alerta(mensagem: String){
-        Toast.makeText(this, mensagem, Toast.LENGTH_LONG).show()
-    }
+    //IRÁ PEGAR OS DADOS NA API
+//    override fun onStart(){
+//        super.onStart();
+//
+//        ForumWebClient().getCategorias(object : ICallbackResponse<List<Categoria>>{
+//            override fun success(categorias: List<Categoria>) {
+//                this@MainActivity.categorias = categorias
+//
+//                for (cat in categorias!!){
+//                    alerta(cat.nome!!)
+//                    Log.i(TAG + "Categoria", cat.toString())
+//
+//                    listView = findViewById(R.id.listView)
+//                }
+//            }
+//        })
+//    }
+
+
+//    //EXIBE RÁPIDAMENTE NA TELA COMO SE FOSSE UM POP-UP
+//    fun alerta(mensagem: String){
+//        Toast.makeText(this, mensagem, Toast.LENGTH_LONG).show()
+//    }
 }
