40653b09e41315b738312fd6301b28f53e4ed4fd
Diogo Leite <diogo.development@gmail.com>
2018-11-19 13:45:21 -0300

    Fiz um monte de coisa que não lembro mais

---
+++ app/src/main/java/br/com/dispositivosmoveis/forumandroid/activity/NovoTopicoActivity.kt
@@ -0,0 +1,12 @@
+package br.com.dispositivosmoveis.forumandroid.activity
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class NovoTopicoActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_novo_topico)
+    }
+}
---
+++ app/src/main/java/br/com/dispositivosmoveis/forumandroid/activity/TopicosActivity.kt
@@ -0,0 +1,12 @@
+package br.com.dispositivosmoveis.forumandroid.activity
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class TopicosActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_topicos)
+    }
+}
